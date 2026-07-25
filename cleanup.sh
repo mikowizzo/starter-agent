67914bd46f161ba0155fc1ad04fa8a08bc468228
AC <mikowizzo@gmail.com>
2026-07-25 10:45:29 +0000

    chore: remove one-off debugging scripts

--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-# quick check
-docker ps -a --filter name=starter2 --format "{{.Names}} {{.Ports}}"
-docker rm -f starter2-frontend-1 starter2-backend-1 2>/dev/null
-docker network rm starter2_default 2>/dev/null
-# what's on 3100?
-docker ps --filter publish=3100 --format "table {{.Names}}\t{{.Ports}}"
